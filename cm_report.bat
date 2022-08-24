d593177f846a962b638d4906b41c7386421c3df1
Everton da Rosa <everton3x@gmail.com>
2022-08-24 16:24:05 -0300

    cm_report.bat updated

--- cm_report.bat
+++ cm_report.bat
@@ -3,6 +3,8 @@
 SET /P ano=Informe o ano [AAAA]
 SET /P mes=Informe o mês [MM]
 
+RScript.exe cm_generator.R
+
 pdflatex -file-line-error -halt-on-error -output-directory output/ -output-format pdf cm_report.tex
 
 SET filename="indicadores cm %ano%-%mes%.pdf"
