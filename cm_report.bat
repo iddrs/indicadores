90208643ef98e5de906f4128f19dd554e87f2f1d
Everton da Rosa <everton3x@gmail.com>
2022-08-24 13:55:14 -0300

    Gerador do report cm para linha de comando

---
+++ cm_report.bat
@@ -0,0 +1,17 @@
+@echo off
+
+SET /P ano=Informe o ano [AAAA]
+SET /P mes=Informe o mês [MM]
+
+pdflatex -file-line-error -halt-on-error -output-directory output/ -output-format pdf cm_report.tex
+
+SET filename="indicadores cm %ano%-%mes%.pdf"
+echo %filename%
+
+cd output
+
+MOVE /Y cm_report.pdf %filename%
+cd ..
+
+pause
+exit
